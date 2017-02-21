c8105cdcc47b44b45573b3a7b88f0bd45edc9705
pierrepayen <payen.pierre@gmail.com>
2017-02-21 10:22:13 +0100

    fix file's sizes

--- cfb/scan.php
+++ cfb/scan.php
@@ -1,43 +1,26 @@
 <?php
-
 $path = "/path/to/folder";
 $dirname = end(explode('/',$path));
-
 // Run the recursive function 
-
 $response = scan($path,$dirname);
-
-
 function getFileExtension($path) { 
   $ext = pathinfo($path, PATHINFO_EXTENSION); 
   return $ext; 
 }
-
 // This function scans the files folder recursively, and builds a large array
-
 function scan($path,$dirname){
-
   $files = array();
-
-
   // Is there actually such a folder/file?
-
   if(file_exists($path)){
-
     foreach(scandir($path) as $f) {
-    
       if(!$f || $f[0] == '.') {
         continue; // Ignore hidden files
       }
-
       if(getFileExtension($f)=='php') {
         continue; // Ignore php
       }
-
       if(is_dir($path . '/' . $f)) {
-
         // The path is a folder
-
         $files[] = array(
           "name" => $f,
           "type" => "folder",
@@ -45,31 +28,21 @@ function scan($path,$dirname){
           "items" => scan($path.'/'.$f, $dirname.'/'.$f) // Recursively get the contents of the folder
         );
       }
-      
       else {
-
         // It is a file
-
         $files[] = array(
           "name" => $f,
           "type" => "file",
           "path" => $dirname.'/'.$f,
-          "size" => filesize($path.'/'.$f, $dirname.'/'.$f) // Gets the size of this file
+          "size" => filesize($path.'/'.$f) // Gets the size of this file
         );
       }
     }
-   
   }
-
   return $files;
 }
-
-
-
 // Output the pathectory listing as JSON
-
 header('Content-type: application/json');
-
 echo json_encode(array(
   "name" => $dirname,
   "type" => "folder",
