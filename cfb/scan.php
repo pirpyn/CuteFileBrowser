38dcb1170b2b90476022ae44d7db30ed05e2d8a8
Pierre Payen <payen.pierre@gmail.com>
2017-05-24 14:03:25 +0200

    Added ignored file as array
    also clarified path as fullpath
    closing the php

--- cfb/scan.php
+++ cfb/scan.php
@@ -1,12 +1,22 @@
 <?php
-$path = "/path/to/folder";
+
+$path = realpath("/absolute/or/relative/path/to/files");
+$ext_to_exclude = array('php','html','.htacces');
+
 $dirname = end(explode('/',$path));
+
 // Run the recursive function 
 $response = scan($path,$dirname);
-function getFileExtension($path) { 
-  $ext = pathinfo($path, PATHINFO_EXTENSION); 
-  return $ext; 
+
+function cmprExtension($file,$exts) {
+  $file_ext = pathinfo($file, PATHINFO_EXTENSION);
+  foreach ($exts as $ext) {
+    if ($file_ext === $ext) {
+      return TRUE;
+    }
+  }
 }
+
 // This function scans the files folder recursively, and builds a large array
 function scan($path,$dirname){
   $files = array();
@@ -16,15 +26,15 @@ function scan($path,$dirname){
       if(!$f || $f[0] == '.') {
         continue; // Ignore hidden files
       }
-      if(getFileExtension($f)=='php') {
-        continue; // Ignore php
+      if(cmprExtension($f,$ext_to_exclude)) {
+        continue; // Ignore some extensions
       }
       if(is_dir($path . '/' . $f)) {
         // The path is a folder
         $files[] = array(
           "name" => $f,
           "type" => "folder",
-          "path" => $dirname.'/' .$f,
+          "path" => $dirname.'/'.$f,
           "items" => scan($path.'/'.$f, $dirname.'/'.$f) // Recursively get the contents of the folder
         );
       }
@@ -41,7 +51,7 @@ function scan($path,$dirname){
   }
   return $files;
 }
-// Output the pathectory listing as JSON
+// Output the listing as JSON
 header('Content-type: application/json');
 echo json_encode(array(
   "name" => $dirname,
@@ -49,3 +59,4 @@ echo json_encode(array(
   "path" => $dirname,
   "items" => $response
 ));
+?>
